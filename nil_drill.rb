b4bc442e196b60cdbd6a604927dd2c915c17ce1a
Bruce Park <bruce@binarywebpark.com>
2014-05-28 16:50:51 -0700

    building nested hashes: 1-D w/default values

--- nil_drill.rb
+++ nil_drill.rb
@@ -1,5 +1,3 @@
-require 'active_support/core_ext/object/blank'
-
 def got_nils?(original, options={flag: false, method: :keys})
   ##method: :keys or :values (of a Hash)
   raise ArgumentError, "no method :keys or :values in options" if !(options[:method]==:keys||options[:method]==:values)
@@ -20,13 +18,48 @@ def nil_substituter(nested_hash, options={default: ""})
   #default key is always an option
   #other keys in options hash should match up to keys in nested_hash if you want to substitute that key's value
   return nil if !nested_hash.is_a?(Hash)
+  options = {default: ""}.merge(options)
   nested_hash.each do |key, value|
     if value==nil
       nested_hash[key] = options[:default] || options[key]
     elsif value.is_a?(Hash)
-      nil_substituter(nested_hash[key])
+      nil_substituter(nested_hash[key], options)
     end
   end
+  nested_hash
+end
+
+def nil_chain_builder(nested_hash, options={default: ""}, *args)
+  #build a 1-D nested hash with default values, assume *args describes nested key order
+  #other keys in options hash should match up to keys in nested_hash if you want to substitute that key's value
+  return if !nested_hash.is_a?(Hash)
+  options = {default: ""}.merge(options)
+  arg = args[0]
+  args = args.drop(1)
+  value = options[arg] || options[:default]
+  args.length > 0 ? nested_hash[arg] = {} : nested_hash[arg] = value
+  nil_chain_builder(nested_hash[arg], options, *args)
+  nested_hash
+end
+
+def nil_chain_substitute_builder(nested_hash, options={default: ""}, *args)
+  #build a 1-D nested hash with default values if no hash/non-nil value exists, otherwise use existing values, assume *args describes nested key order
+  #other keys in options hash should match up to keys in nested_hash if you want to substitute that key's value
+  return if !nested_hash.is_a?(Hash)
+  options = {default: ""}.merge(options)
+  arg = args[0]
+  args = args.drop(1)
+  value = options[arg] || options[:default]
+  if args.length>0
+    if nested_hash[arg].nil?
+      nested_hash[arg] = {}
+    end
+  else
+    if nested_hash[arg].nil?
+      nested_hash[arg] = value
+    end
+  end
+  nil_chain_substitute_builder(nested_hash[arg], options, *args)
   nested_hash
 end
 
@@ -42,13 +75,16 @@ def nested_hash_keys(nested_hash)
   keys.flatten!.reject { |k| k==nil }
 end
 
-g = {:a=>{:b=>nil, :c=>{:d=>nil}}, :e=>{:f=>{:g=>{:h=>nil}}}}
+# g = {:a=>{:b=>nil, :c=>{:d=>nil}}, :e=>{:f=>{:g=>{:h=>nil}}}}
+# p nil_substituter(g, options={default: ""})
 
-# c = [:a, :b]
-# p nil_substituter(g, options={default: ""}, *c)
+# h = {:a=>1, :b=>2, :d=>{:c=>nil}}
+# p got_nils?(h, options = {flag: false, method: :values})
 
-p nil_substituter(g, options={default: ""})
+# i = {"a"=>{"b"=>{"c"=>nil}}}
+# c = nested_hash_keys(i)
+# p nil_chain_builder(i, options={default: "", "c"=>55}, *c)
 
-h = {:a=>1, :b=>2, :d=>{:c=>nil}}
-p got_nils?(h, options = {flag: false, method: :values})
-
+i = {"a"=>{"b"=>{"c"=>nil}}}
+c = ["a","b","c","d"]
+p nil_chain_substitute_builder(i, options={default: "", "c"=>55}, *c)
