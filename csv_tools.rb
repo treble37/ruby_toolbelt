9f3d53b76e0bcd482d34d5571532d12642495bc9
Bruce Park <brucepark0@gmail.com>
2014-07-03 10:57:41 -0700

    csv_tools.rb

---
+++ csv_tools.rb
@@ -0,0 +1,17 @@
+require 'csv'
+
+class CSVTools
+  def hash_to_csv_file(h, file_name="test.csv", options={columns: 1, headers: [], write_headers: false})
+    #hash keys are 1 column, hash values are 1 column; if array of multiple hash values, you can specify whether you want them to be spread across multiple columns
+    CSV.open(file_name, "w+", write_headers: options[:write_headers], headers: options[:headers]) do |csv|
+      h.each do |key, value|
+        row = key.to_s
+        v = value.is_a?(Array) ? value.join(",") : value.to_s
+        row = [row, v]
+        csv << row
+      end
+    end
+    
+  end
+end
+
