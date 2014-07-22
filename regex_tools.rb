6985da33e72c8c543047d61bd53c922a152448b5
Bruce Park <brucepark0@gmail.com>
2014-07-22 02:58:27 -0700

    add email validation

--- regex_tools.rb
+++ regex_tools.rb
@@ -7,4 +7,10 @@ class RegexTools
     m = num.to_s =~ /([0-9]*)?\.[0-9]+/
     !m.nil?
   end
+  def valid_email?(email)
+    email = email.downcase
+    m = email =~ /([a-zA-z0-9_-])+@([a-zA-Z])+\.([a-zA-z])+/
+    !m.nil?
+  end
 end
+
